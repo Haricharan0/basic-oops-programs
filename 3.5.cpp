c3cb84694d7d696ed2f19e902a7711626d783475
Haricharan0 <101371119+Haricharan0@users.noreply.github.com>
2022-10-17 11:08:20 +0530

    Add files via upload

---
+++ 3.5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+	int intvar=25;
+	float floatvar=54.32;
+	
+	cout<<"intvar = "<<intvar;
+	cout<<"\nfloatvar = "<<floatvar;
+	cout<<"\nfloat(intvar) = "<<float(intvar);
+	cout<<"\nint(floatvar) = "<<int(floatvar);
+	
+	getch();
+}
+
+
